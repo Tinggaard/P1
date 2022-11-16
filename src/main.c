e9400c268669eacdc9a6aab4c2943a532a11997a
T1ng <mail@t1ng.dk>
2022-11-16 13:16:27 +0100

    added structs for base setup

--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
+typedef struct {
+    char name[20];
+    int price;
+} item_s;
+
+typedef struct node_t {
+    item_s item;
+    struct node_t* next;
+} node_t;
+
+typedef struct {
+    char name[20];
+    int distance;
+    node_t* first_item;
+} store_s;
+
+typedef struct {
+    store_s store;
+    item_s item;
+} store_item;
+
 int main(void) {
     return 0;
 }
